28c9ea9cb4da70578067e312c145c21b2d580a4d
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2020-09-03 22:19:34 +0530

    Update FileFromTo.java

--- 4/FileFromTo.java
+++ 4/FileFromTo.java
@@ -1,9 +1,5 @@
 import org.apache.commons.io.FileUtils;
 
-import java.nio.charset.StandardCharsets;
-
-import org.apache.commons.io.FileUtils;
-
   private static void stringToFile(String fullDocument) throws IOException {
 
     FileUtils.writeStringToFile(new File("/some/dir/filename.txt"), fullDocument, StandardCharsets.UTF_8);
@@ -12,7 +8,7 @@ import org.apache.commons.io.FileUtils;
 	
   private static String fileToString(String fileName)  throws IOException  {	
   
-    return FileUtils.readFileToString(new File(fileName), StandardCharsets.UTF_8);
+    return FileUtils.readFileToString(new File(fileName));
   
   }
 
