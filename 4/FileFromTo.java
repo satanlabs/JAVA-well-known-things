e3f208bfb9104c1bfd6b13876fa253cc3d5cb554
arsh-ola <113987187+arsh-ola@users.noreply.github.com>
2022-09-20 18:41:44 +0530

    Update FileFromTo.java

--- 4/FileFromTo.java
+++ 4/FileFromTo.java
@@ -1,17 +1,13 @@
 import org.apache.commons.io.FileUtils;
 
-  private static void stringToFile(String fullDocument) throws IOException {
-
-    FileUtils.writeStringToFile(new File("/some/dir/filename.txt"), fullDocument, StandardCharsets.UTF_8);
-
+  private static void stringToFile(String fileUrl, String fullDocument) throws IOException {
+    FileUtils.writeStringToFile(new File(fileUrl), fullDocument, StandardCharsets.UTF_8);
   }
 	
-  private static String fileToString(String fileName)  throws IOException  {	
-  
-    return FileUtils.readFileToString(new File(fileName));
-  
+  private static String fileToString(String fileUrl)  throws IOException  {	
+    return FileUtils.readFileToString(new File(fileUrl));
   }
 
-private static byte[] fileToByteArray(String fileName) throws IOException  {
-	return FileUtils.readFileToByteArray(new File(fileName));
+private static byte[] fileToByteArray(String fileUrl) throws IOException  {
+	return FileUtils.readFileToByteArray(new File(fileUrl));
 }
